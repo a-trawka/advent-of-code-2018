def175742592be0077f21bccb6e33c1349644978
Adrian <ad.trawka@gmail.com>
2018-12-02 14:05:28 +0100

    fix common :/

--- src/main/scala/common/package.scala
+++ src/main/scala/common/package.scala
@@ -3,7 +3,7 @@ import scala.io.Source
 package object common {
 
   def puzzleInput(resourceName: String): List[String] = {
-    val bufferedSource = Source.fromResource("day01.txt")
+    val bufferedSource = Source.fromResource(resourceName)
     val linesList = bufferedSource.getLines.toList
     bufferedSource.close
     linesList
